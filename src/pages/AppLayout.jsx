fb2dae4a3f2c6db3fcdc23cb4dfa56b9eda27ccf
aleksandrawozniak965 <aleksandra.wozniak965@gmail.com>
2024-06-20 19:53:06 +0200

    Homepage and nav

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/APPNAV.JSX";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
